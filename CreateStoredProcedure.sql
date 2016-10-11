ecae04adb9d7d8c365a1864687b9d5d675fdb161
John Lopez <johnalbh@gmail.com>
2016-10-11 13:55:19 -0500

    Formato Creación SP
    Sintaxis básica para la creación de un SP.

---
+++ CreateStoredProcedure.sql
@@ -0,0 +1,29 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/********************************************************************
+NOMBRE:				dbo.PR_SGS_Rpt_ListadoGeneralVehiculos.sql
+DESCRPCIÓN:			Creación Strored Procedures "Consulta Listado General Vehículos"
+AUTOR:				John Alberto López Hernández
+REQUERIMIENTO:		SP38 - Transporte
+EMPRESA:			Colegio San Jorge de Inglaterra
+FECHA CREACIÓN:		11/10/2016
+PARÁMETROS ENTRADA:	No Aplica
+EXCEPCIONES:		No Aplica
+---------------------------------------------------------------------
+MODIFICACIÓN:
+AUTOR:
+REQUERIMIENTO:
+EMPRESA:
+FECHA MODIFICACIÓN:
+********************************************************************/
+CREATE PROCEDURE 
+	 [dbo].[PR_SGS_Rpt_ListadoGeneralVehiculos] 
+AS
+BEGIN
+
+
+
+END
+GO
