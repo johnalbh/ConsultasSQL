030d987fe572a79104d5ded9cbe512da977bdd2a
John Lopez <johnalbh@gmail.com>
2016-12-01 15:53:47 -0500

    Modificación
    Modificación

--- CreateStoredProcedure.sql
+++ CreateStoredProcedure.sql
@@ -3,24 +3,31 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 /********************************************************************
-NOMBRE:				dbo.PR_SGS_Rpt_InformeAtencionCADI.sql
-DESCRPCIÓN:			Creación Strored Procedures "Informe Atención por 
-					rango de fechas atención CADI"
-AUTOR:				John Alberto López Hernández
-REQUERIMIENTO:		SP40 - Transporte
-EMPRESA:			Colegio San Jorge de Inglaterra
-FECHA CREACIÓN:		11/11/2016
-PARÁMETROS ENTRADA:	No Aplica
-EXCEPCIONES:		No Aplica
----------------------------------------------------------------------
-MODIFICACIÓN:
-AUTOR:
-REQUERIMIENTO:
-EMPRESA:
-FECHA MODIFICACIÓN:
-********************************************************************/
-CREATE PROCEDURE 
-	 [dbo].[PR_SGS_Rpt_InformeAtencionCADI] 
+NOMBRE:					dbo.PR_SGS_Rpt_RegistroInscripcionGrupoEstudiante.sql
+DESCRIPCIÓN:			Muestra los Grupos a los que esta inscrito un estudiante.
+						Reportes de Estudiantes Inscritos a Grupos por Día   
+						Devuelve un listado de todos los grupos en los que está inscrito un estudiante en una fecha dada.
+PARÁMETROS DE ENTRADA:  @dP_Fecha: Fecha de la consulta 
+PARÁMETROS DE SALIDA:   No aplica.  
+RESULTADO:				Listado con las siguientes columnas: 
+						Seccion, Nivel, Curso, Apellidos, Nombres, Grupo, FechaInicio, FechaFin, Profesor,Inactivo 
+						(* si el estudiante está inactivo, vacío si está activo).
+						La consulta se entrega ordenada alfabéticamente por apellidos y nombres del estudiante y por nombre del grupo	
+CREACIÓN 
+REQUERIMIENTO:			Reportes de Transporte
+AUTOR:					LLamprea
+EMPRESA:				Saint George´s School  
+FECHA DE CREACIÓN:		2016-08-27
+----------------------------------------------------------------------------
+****************************************************************************/
+ALTER PROCEDURE 
+	 [dbo].[PR_SGS_Rpt_RegistroInscripcionGrupoEstudiante] 
+
+		 @dp_Fecha DATE		
+		,@Seccion INT
+		,@Nivel VARCHAR(60)
+		,@Curso VARCHAR (60)
+
 AS
 BEGIN
 
