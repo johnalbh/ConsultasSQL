9cd5764a8ed3775220f0dfb06e58e3b56151a7cb
John Lopez <johnalbh@gmail.com>
2016-12-15 15:08:17 -0500

    Script Creación Departamentos Academicos
    Script para la creación de departamentos académicos.

---
+++ CargaDepartamentosAcademicos.sql
@@ -0,0 +1,29 @@
+SET IDENTITY_INSERT DepartamentoAcademico ON
+Insert into DepartamentoAcademico (Id, Nombre, TipoDocumentoEmpleado, NumeroDocumentoEmpleado) values
+(1,'Rectoría','CC','19094863'),
+(2,'Vicerrectoría','CC','51773559'),
+(3,'Departamento de Sistemas','CC','41791463'),
+(4,'Coordinación de Bachillerato','CC','52006580'),
+(5,'Coordinación de Primaria','CE','391392'),
+(6,'Coordinación de Preescolar ','CC','39683779'),
+(7,'Coordinación de Consejería Escolar','CC','52250112'),
+(8,'Coordinación de Disciplina','CC','19252671'),
+(9,'Departamento de Religión','CC','35394273'),
+(10,'Departamento de Educación Física ','CC','52796794'),
+(11,'Departamento de Español ','CE','323563'),
+(12,'Departamento de Música','CC','79391470'),
+(13,'Departamento de Arte ','CC','79522560'),
+(14,'Departamento de Sociales','CC','66996675'),
+(15,'Departamento de Inglés','CE','346821'),
+(16,'Departamento de Geografía','CC','52394832'),
+(17,'Departamento de Matemáticas','CC','6761867'),
+(18,'Departamento de Ciencias','CC','52097781'),
+(19,'Servicio de Alimentación','CC','41746713'),
+(20,'Gestión Ambiental','CC','52699230'),
+(21,'Gestión Financiera','CC','1019021359'),
+(22,'Centro de Aprendizaje y Desarrollo Integral','CC','52694097'),
+(23,'Departamento de Francés','CC','79489696'),
+(24,'Consejería Escolar','CC','52250112')
+SET IDENTITY_INSERT DepartamentoAcademico OFF
+
+
